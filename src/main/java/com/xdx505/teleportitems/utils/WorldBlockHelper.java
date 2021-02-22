1ab5fa4aa6ea7a609a9b3f6c105ff033659d26eb
Dan Shadrin (xdx505) <shadrin.smr@gmail.com>
2021-02-22 18:07:29 +0400

    First commit

---
+++ src/main/java/com/xdx505/teleportitems/utils/WorldBlockHelper.java
@@ -0,0 +1,14 @@
+package com.xdx505.teleportitems.utils;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class WorldBlockHelper {
+
+    public static boolean isPlayerCapaciousBlock(World worldIn, DimensionBlockPos pos) {
+        if (!worldIn.isAirBlock(pos)) {
+            return false;
+        }
+        return worldIn.isAirBlock(new BlockPos(pos.getX(), pos.getY() + 1, pos.getZ()));
+    }
+}
