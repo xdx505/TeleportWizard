cdf78f5fb6dd185445cf234061cf44b4abdde0a0
Dan Shadrin (xdx505) <shadrin.smr@gmail.com>
2021-02-22 18:08:15 +0400

    Change tp method

--- src/main/java/com/xdx505/teleportitems/utils/TeleportUtils.java
+++ src/main/java/com/xdx505/teleportitems/utils/TeleportUtils.java
@@ -1,15 +1,16 @@
 package com.xdx505.teleportitems.utils;
 
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
 public class TeleportUtils {
 
-    public static void teleport(PlayerEntity player, DimensionBlockPos pos) {
+    public static void teleport(PlayerEntity playerIn, DimensionBlockPos pos) {
         if (pos.getDimensionType() != null) {
             //TODO change dimension type
         }
-        player.setPosition(pos.getX(), pos.getY() + 1, pos.getZ());
+        playerIn.teleportKeepLoaded(pos.getX(), pos.getY(), pos.getZ());
     }
 }
