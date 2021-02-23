6ddba2125b76ab1e7c3a3d3fbc76f2b6f698ec90
Dan Shadrin (xdx505) <shadrin.smr@gmail.com>
2021-02-23 20:16:52 +0400

    Add sendMessage

--- src/main/java/com/xdx505/teleportitems/utils/TeleportUtils.java
+++ src/main/java/com/xdx505/teleportitems/utils/TeleportUtils.java
@@ -1,11 +1,11 @@
 package com.xdx505.teleportitems.utils;
 
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.server.command.TextComponentHelper;
+
+import java.util.UUID;
 
 public class TeleportUtils {
 
@@ -16,15 +16,9 @@ public class TeleportUtils {
 
         }
 
-        DimensionBlockPos emptyPosition = WorldBlockHelper.findEmptySpaceByY(worldIn, pos);
+        final DimensionBlockPos emptyPosition = WorldBlockHelper.findEmptySpaceByY(worldIn, pos);
         if (emptyPosition != null) {
             playerIn.teleportKeepLoaded(emptyPosition.getX(), emptyPosition.getY(), emptyPosition.getZ());
-        }
-
-        SoundEvent event = new SoundEvent(new ResourceLocation("entity.enderman.teleport"));
-        if (event == null) {
-            return;
-        }
-        worldIn.playSound(null, pos.getX(), pos.getY(), pos.getZ(), event, SoundCategory.BLOCKS, 1.0f, 1.0f);
+        } else playerIn.sendMessage(TextComponentHelper.createComponentTranslation(playerIn, "teleportitems.teleporting_noemptyspace"),UUID.randomUUID());
     }
 }
