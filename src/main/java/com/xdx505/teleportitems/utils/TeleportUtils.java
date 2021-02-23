2a94734c1fca4614a66ee427d31573dbf6a18462
Dan Shadrin (xdx505) <shadrin.smr@gmail.com>
2021-02-23 12:36:08 +0400

    Update teleport method

--- src/main/java/com/xdx505/teleportitems/utils/TeleportUtils.java
+++ src/main/java/com/xdx505/teleportitems/utils/TeleportUtils.java
@@ -1,16 +1,30 @@
 package com.xdx505.teleportitems.utils;
 
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
-
-import javax.annotation.Nullable;
+import net.minecraft.world.server.ServerWorld;
 
 public class TeleportUtils {
 
-    public static void teleport(PlayerEntity playerIn, DimensionBlockPos pos) {
-        if (pos.getDimensionType() != null) {
+    public static void teleport(World worldIn, PlayerEntity playerIn, DimensionBlockPos pos) {
+        if (!playerIn.getEntityWorld().getDimensionType().equals(pos.getDimensionType())) {
             //TODO change dimension type
+            playerIn.changeDimension((ServerWorld) worldIn);
+
         }
-        playerIn.teleportKeepLoaded(pos.getX(), pos.getY(), pos.getZ());
+
+        DimensionBlockPos emptyPosition = WorldBlockHelper.findEmptySpaceByY(worldIn, pos);
+        if (emptyPosition != null) {
+            playerIn.teleportKeepLoaded(emptyPosition.getX(), emptyPosition.getY(), emptyPosition.getZ());
+        }
+
+        SoundEvent event = new SoundEvent(new ResourceLocation("entity.enderman.teleport"));
+        if (event == null) {
+            return;
+        }
+        worldIn.playSound(null, pos.getX(), pos.getY(), pos.getZ(), event, SoundCategory.BLOCKS, 1.0f, 1.0f);
     }
 }
