350011b430a269b8e50c9a214aa3acdac9bf9191
Dan Shadrin (xdx505) <shadrin.smr@gmail.com>
2021-02-23 23:00:07 +0400

    First commit

---
+++ src/main/java/com/xdx505/teleportitems/storage/models/TeleportItemsConfig.java
@@ -0,0 +1,22 @@
+package com.xdx505.teleportitems.storage.models;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class TeleportItemsConfig {
+    public static final ForgeConfigSpec CONFIG;
+    public static ForgeConfigSpec.ConfigValue<Integer> teleportDelay;
+
+    static {
+        ForgeConfigSpec.Builder configBuilder = new ForgeConfigSpec.Builder();
+        init(configBuilder);
+        CONFIG = configBuilder.build();
+    }
+
+    private static void init(ForgeConfigSpec.Builder config) {
+        config.comment("Mod config file.\n\n");
+
+        teleportDelay = config
+                .comment("Delay before teleport in seconds")
+                .define("delay", 3);
+    }
+}
