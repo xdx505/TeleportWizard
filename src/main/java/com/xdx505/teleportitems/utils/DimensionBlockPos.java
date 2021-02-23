775a811f974bde5cfb312e6eb08ff87a54f5d107
Dan Shadrin (xdx505) <shadrin.smr@gmail.com>
2021-02-23 20:17:31 +0400

    Refactor code

--- src/main/java/com/xdx505/teleportitems/utils/DimensionBlockPos.java
+++ src/main/java/com/xdx505/teleportitems/utils/DimensionBlockPos.java
@@ -1,10 +1,7 @@
 package com.xdx505.teleportitems.utils;
 
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.DimensionType;
-
-import javax.swing.text.html.parser.Entity;
 
 public class DimensionBlockPos extends BlockPos {
     private final DimensionType dimensionType;
