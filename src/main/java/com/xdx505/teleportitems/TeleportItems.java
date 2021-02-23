6d0f9b5b71fcc6c1bd4359c3fb57dd2dc552638b
Dan Shadrin (xdx505) <shadrin.smr@gmail.com>
2021-02-23 23:00:59 +0400

    Add TickHandler

--- src/main/java/com/xdx505/teleportitems/TeleportItems.java
+++ src/main/java/com/xdx505/teleportitems/TeleportItems.java
@@ -1,8 +1,12 @@
 package com.xdx505.teleportitems;
 
+import com.xdx505.teleportitems.handlers.TickHandler;
 import com.xdx505.teleportitems.setup.Registration;
+import com.xdx505.teleportitems.storage.models.TeleportItemsConfig;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,10 +14,12 @@ import org.apache.logging.log4j.Logger;
 public class TeleportItems {
     public static final String MODID = "teleportitems";
     private static TeleportItems INSTANCE;
-    private final Logger logger = LogManager.getLogger();
+    private Logger logger = LogManager.getLogger();
+    private TickHandler tickHandler = new TickHandler();
 
     public TeleportItems() {
         INSTANCE = this;
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, TeleportItemsConfig.CONFIG);
         Registration.getInstance();
         MinecraftForge.EVENT_BUS.register(this);
     }
@@ -24,5 +30,9 @@ public class TeleportItems {
 
     public Logger getLogger() {
         return logger;
+    }
+
+    public TickHandler getTickHandler() {
+        return tickHandler;
     }
 }
