1beaca8a8f6687ada5ba7e61e1992654d2bc8172
Dan Shadrin (xdx505) <shadrin.smr@gmail.com>
2021-02-23 20:14:51 +0400

    Move ender tp sound

--- src/main/java/com/xdx505/teleportitems/items/teleportation/BaseTeleportationItem.java
+++ src/main/java/com/xdx505/teleportitems/items/teleportation/BaseTeleportationItem.java
@@ -23,21 +23,27 @@ public abstract class BaseTeleportationItem extends Item {
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
         ItemStack stack = playerIn.getHeldItem(handIn);
 
-        SoundEvent event = new SoundEvent(new ResourceLocation("entity.splash_potion.break"));
-        if (event != null) {
-            worldIn.playSound(null, playerIn.getPosX(), playerIn.getPosY(), playerIn.getPosZ(), event, SoundCategory.BLOCKS, 1.0f, 1.0f);
+        final SoundEvent eventGlass = new SoundEvent(new ResourceLocation("entity.splash_potion.break"));
+        if (eventGlass != null) {
+            worldIn.playSound(null, playerIn.getPosX(), playerIn.getPosY(), playerIn.getPosZ(), eventGlass, SoundCategory.BLOCKS, 1.0f, 1.0f);
         }
 
         stack.shrink(1);
 
-        DimensionBlockPos portalPoint;
+        final DimensionBlockPos portalPoint;
         try {
             portalPoint = getPortalPoint(playerIn);
         } catch (MinecraftTextFormattedException e) {
             playerIn.sendMessage(e.getReason(), UUID.randomUUID());
             return new ActionResult<>(ActionResultType.FAIL, stack);
         }
+
         teleport(worldIn, playerIn, portalPoint);
+
+        final SoundEvent eventEnder = new SoundEvent(new ResourceLocation("entity.enderman.teleport"));
+        if (eventEnder != null) {
+            worldIn.playSound(null, playerIn.getPosX(), playerIn.getPosY(), playerIn.getPosZ(), eventEnder, SoundCategory.BLOCKS, 1.0f, 1.0f);
+        }
 
         return new ActionResult<>(ActionResultType.SUCCESS, stack);
     }
